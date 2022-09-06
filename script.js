c4e55fd8f850d316725e65398c6884f3dd6e2f4e
shakerAbuDrais <thecoffeiner@gmail.com>
2022-09-06 16:41:32 +0300

    Add html and js for the curtain menu

---
+++ script.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+function openNav() {
+  document.getElementById('myNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.display = 'none';
+}
